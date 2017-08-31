fa0501f3f36e7222f75fc38b25782350b5b53ca8
Moeinh77 <moeinhasani77@gmail.com>
2017-08-31 22:17:18 +0430

    signup function

--- app/src/main/java/com/taan/hasani/moein/volley/MainActivity.java
+++ app/src/main/java/com/taan/hasani/moein/volley/MainActivity.java
@@ -18,7 +18,7 @@ import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    String url = "http://jsonplaceholder.typicode.com/users";
+    String url = "http://online6732.tk/guessIt.php";
     HashMap<String, String> info = new HashMap<String, String>();
     private String password;
     private String username;
@@ -29,20 +29,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void sign_up(){
+
         info.put("action","signup");
         info.put("password",password);
         info.put("username",username);
         info.put("name",name);
         jsonObject=new JSONObject(info);
 
-
         JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.POST,
                 url, jsonObject,new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
+            @Override
+            public void onResponse(JSONObject response) {
 
-                    }
-                }, new Response.ErrorListener() {
+                try {
+
+                    JSONArray jsonArray = response.getJSONArray("weather");
+                    JSONObject object = jsonArray.getJSONObject(0);
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
 
