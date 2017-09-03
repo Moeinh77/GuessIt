2308e153c8c28634baac3592a7eeda5041341f19
Moeinh77 <moeinhasani77@gmail.com>
2017-09-03 23:46:14 +0430

    loading with progressbar

--- app/src/main/java/com/taan/hasani/moein/volley/game_menu/profile.java
+++ app/src/main/java/com/taan/hasani/moein/volley/game_menu/profile.java
@@ -11,6 +11,5 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
     }
 }
