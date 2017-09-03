652152762444611500c44937284ae28ba8e32244
Moeinh77 <moeinhasani77@gmail.com>
2017-09-03 23:45:14 +0430

    loading with progressbar

--- app/src/main/java/com/taan/hasani/moein/volley/game_menu/Loading.java
+++ app/src/main/java/com/taan/hasani/moein/volley/game_menu/Loading.java
@@ -2,6 +2,7 @@ package com.taan.hasani.moein.volley.game_menu;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -21,6 +22,7 @@ import java.util.HashMap;
 
 public class Loading extends AppCompatActivity {
 
+    private static int TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +30,25 @@ public class Loading extends AppCompatActivity {
         setContentView(R.layout.activity_loading);
         final String MY_PREFS_NAME="username and password";
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+                String username = prefs.getString("usename", null);
+                String password = prefs.getString("password", null);
+                if (username != null &&  password!=null) {
+
+                    // Toast.makeText(getApplicationContext(),username+","+password,Toast.LENGTH_LONG).show();
+                    sending_info(username,password);
+
+                }
+            }
+        }, TIME_OUT);
+
+
         //check for login
-        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
-        String username = prefs.getString("usename", null);
-        String password = prefs.getString("password", null);
-        if (username != null &&  password!=null) {
 
-            // Toast.makeText(getApplicationContext(),username+","+password,Toast.LENGTH_LONG).show();
-            sending_info(username,password);
-
-        }
     }
-
 
     public void sending_info(String username_, String password_){
         final HashMap<String, String> info = new HashMap<>();
